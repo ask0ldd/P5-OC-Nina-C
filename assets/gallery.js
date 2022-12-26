1ed5738302d7f9fa13d10fd7574bcc1c06940230
Cedric B-Eff <cedrikopenclass@gmx.com>
2022-12-26 05:09:38 +0100

    gallery class implemented

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,127 +1,154 @@
 /*--Gallery--*/
-const oldCloseMethod = HTMLDialogElement.prototype.close
-const oldShowModalMethod = HTMLDialogElement.prototype.showModal
-let modalePic
-let activeFilter = "Tous"
-let currentPictureIndex = 0
-
-HTMLDialogElement.prototype.close = function()
+class ModaleGallery 
 {
-  this.style.display = "none"
-  oldCloseMethod.apply(this)
-}
-
-HTMLDialogElement.prototype.showModal = function(picSrc, index)
-{
-  currentPictureIndex = index
-  modalePic.src=picSrc
-  oldShowModalMethod.apply(this)
-  this.style.display = "flex"
-}
-
-class gallery {
-  constructor(modale, imgNodes) {
-  }
-}
-
-function previousPic(e, pictures)
-{
-  e.stopPropagation()
-  e.preventDefault();
-  do
-  {
-    currentPictureIndex > 0 ? currentPictureIndex-- : currentPictureIndex = pictures.length-1
-    if (activeFilter === "Tous") {break;}
-  }
-  while(pictures[currentPictureIndex].dataset.galleryTag !== activeFilter)
-  
-  modalePic.src=pictures[currentPictureIndex].src
-}
-
-function nextPic(e, pictures)
-{
-  e.stopPropagation()
-  e.preventDefault();
-  do
-  {
-    currentPictureIndex < pictures.length-1 ? currentPictureIndex++ : currentPictureIndex = 0
-    if (activeFilter === "Tous") {break;}
-  }
-  while(pictures[currentPictureIndex].dataset.galleryTag !== activeFilter)
-
-  modalePic.src=pictures[currentPictureIndex].src
-}
-
-function parseFilters(pictures){
-  let categories = []
-  categories.push("Tous")
-  Array.from(pictures).forEach(pic => {
-    if(!categories.includes(pic.dataset.galleryTag))
+    constructor(modale, picturesParentNode) 
     {
-        categories.push(pic.dataset.galleryTag)
+        this.modale = modale
+        modale.addEventListener("click", e => this.closeModale())
+        this.modalePic = this.addModalePic()
+
+        this.picsParentNode = picturesParentNode
+        this.picNodes = picturesParentNode.children
+        this.addListenersToPics()
+
+        this.categories = this.#parseCategories(this.picNodes)
+        this.filtersContainer = document.createElement("ul")
+        this.activeFilter = "Tous"
+        this.currentPictureIndex = 0
+        this.displayFilters()
+
+        this.previousButton = document.querySelector("#previousButton")
+        this.nextButton = document.querySelector("#nextButton")
+        this.previousButton.addEventListener("click", e => this.previousPic(e))
+        this.nextButton.addEventListener("click", e => this.nextPic(e))
     }
-  })
 
-  return categories
-}
+    addModalePic()
+    {
+        const pic = document.createElement("img")
+        pic.setAttribute('id','modalePic')
+        pic.classList.add("modalPic")
+        this.modale.prepend(pic)
+        return pic
+    }
 
-function makeFilterButtonActive(filtersContainer, filter)
-{
-  Array.from(filtersContainer.children).forEach(fil => fil.classList.remove("active"))
-  filter.classList.add("active")
-}
+    #parseCategories(pictures) 
+    {
+        let cat = []
+        cat.push("Tous")
+        Array.from(pictures).forEach(pic => {
+          if(!cat.includes(pic.dataset.galleryTag))
+          {
+              cat.push(pic.dataset.galleryTag)
+          }
+        })
+        return cat
+    }
 
-function PicturesFiltering(){
-  const pictures = document.querySelector("#pictures").children
-  if (activeFilter==="Tous") {
-    Array.from(pictures).forEach(pic => pic.style.display="block")
-    return
-  }
-  Array.from(pictures).forEach(pic => pic.dataset.galleryTag === activeFilter ? pic.style.display="block" : pic.style.display="none" )
-}
+    picsFiltering()
+    {
+        if (this.activeFilter==="Tous") {
+          Array.from(this.picNodes).forEach(pic => pic.style.display="block")
+          return
+        }
+        Array.from(this.picNodes).forEach(pic => pic.dataset.galleryTag === this.activeFilter ? pic.style.display="block" : pic.style.display="none" )
+    }
 
-function displayFilterButtons(filters){
-  const galleryContainer = document.querySelector("#pictures").parentElement
-  const filtersContainer = document.createElement("ul")
-  filtersContainer.classList.add("my-4", "tags-bar", "nav", "nav-pills")
-  filters.forEach(filter => {
-    const filterEl = document.createElement("li")
-    if(filter === activeFilter) filterEl.classList.add("active")
-    filterEl.classList.add("nav-item", "nav-link")
-    filterEl.innerHTML = filter
-    filterEl.addEventListener("click", () => {
-      makeFilterButtonActive(filtersContainer, filterEl)
-      activeFilter = filter
-      console.log(activeFilter)
-      PicturesFiltering()
-    })
-    filtersContainer.append(filterEl)
-  })
-  galleryContainer.prepend(filtersContainer)
-}
+    makeFilterButtonActive(selectedFilter)
+    {
+        Array.from(this.filtersContainer.children).forEach(fil => fil.classList.remove("active"))
+        selectedFilter.classList.add("active")
+    }
 
-function addModalePic(){
-  const pic = document.createElement("img")
-  pic.setAttribute('id','modalePic')
-  pic.classList.add("modalPic")
-  modale.prepend(pic)
-  return pic
+    displayFilters()
+    {
+        this.filtersContainer.classList.add("my-4", "tags-bar", "nav", "nav-pills")
+        this.categories.forEach(filter => {
+          const filterEl = document.createElement("li")
+          if(filter === this.activeFilter) filterEl.classList.add("active")
+          filterEl.classList.add("nav-item", "nav-link")
+          filterEl.innerHTML = filter
+          filterEl.addEventListener("click", () => {
+            this.makeFilterButtonActive(filterEl)
+            this.activeFilter = filter
+            this.picsFiltering()
+          })
+          this.filtersContainer.append(filterEl)
+        })
+        this.picsParentNode.parentElement.prepend(this.filtersContainer)
+    }
+
+    addListenersToPics()
+    {
+        Array.from(this.picNodes).forEach((pic, index) => pic.addEventListener("click", e => this.showModale(pic.src, index)))
+    }
+
+    setModalePic(picture)
+    {
+        console.log(picture)
+        this.modalePic.src=picture
+    }
+
+    #scrollLock(bool = false)
+    {
+        if(bool)
+        {
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+            let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+            window.onscroll = () => {
+                window.scrollTo(scrollLeft, scrollTop)
+            }
+        }else{
+            window.onscroll = () => {}
+        }
+    }
+
+    showModale(picSrc, index)
+    {
+        this.#scrollLock(true)
+        console.log(picSrc)
+        this.currentPictureIndex = index
+        this.setModalePic(picSrc)
+        this.modale.showModal()
+        this.modale.style.display = "flex"        
+    }
+
+    closeModale()
+    {
+        this.modale.style.display = "none"
+        this.modale.close()
+        this.#scrollLock(false)
+    }
+
+    previousPic(e)
+    {
+        e.stopPropagation()
+        e.preventDefault();
+        do
+        {
+            this.currentPictureIndex > 0 ? this.currentPictureIndex-- : this.currentPictureIndex = this.picNodes.length-1
+            if (this.activeFilter === "Tous") {break;}
+        }
+        while(this.picNodes[this.currentPictureIndex].dataset.galleryTag !== this.activeFilter)
+        
+        this.modalePic.src=this.picNodes[this.currentPictureIndex].src
+    }
+
+    nextPic(e)
+    {
+        e.stopPropagation()
+        e.preventDefault();
+        do
+        {
+            this.currentPictureIndex < this.picNodes.length-1 ? this.currentPictureIndex++ : this.currentPictureIndex = 0
+            if (this.activeFilter === "Tous") {break;}
+        }
+        while(this.picNodes[this.currentPictureIndex].dataset.galleryTag !== this.activeFilter)
+
+        this.modalePic.src=this.picNodes[this.currentPictureIndex].src
+    }
 }
 
 function onloadIndex(){
-  const modale = document.querySelector("#modale")
-  modalePic = addModalePic()
-
-  //deal with children = 0
-  const pictures = document.querySelector("#pictures").children
-  Array.from(pictures).forEach((pic, index) => pic.addEventListener("click", e => modale.showModal(pic.src, index)))
-  modale.addEventListener("click", e => modale.close())
-
-  const filters = parseFilters(pictures)
-  displayFilterButtons(filters)
-
-  const previousButton = document.querySelector("#previousButton")
-  const nextButton = document.querySelector("#nextButton")
-  previousButton.addEventListener("click", e => previousPic(e, pictures))
-  nextButton.addEventListener("click", e => nextPic(e, pictures))
+  const gallery = new ModaleGallery(document.querySelector("#modale"), document.querySelector("#pictures"))
 }
